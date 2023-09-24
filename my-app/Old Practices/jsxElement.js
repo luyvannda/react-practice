bd9ab549e0031ffcc49b5c842530b48a699d48f8
luyvannda <philips.joe329@gmail.com>
2023-09-24 11:07:46 +0700

    start new React practice

--- my-app/Old Practices/jsxElement.js
+++ my-app/Old Practices/jsxElement.js
@@ -3,14 +3,34 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 // import App from './App';
 import reportWebVitals from './reportWebVitals';
-import App from './App';
+
+
+
+const jsxElements = (
+  <div>
+    <h1>Hello React!!!</h1>
+    <p>I am the first paragraph</p>
+    <p>How are you doing?</p>
+    <p>Let me love you.</p>
+  </div>
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {jsxElements}
   </React.StrictMode>
 );
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+
+
 
 
 // If you want to start measuring performance in your app, pass a function

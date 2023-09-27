6257a365d7d24fb7619f232c60be83e0622ed152
luyvannda <philips.joe329@gmail.com>
2023-09-27 15:05:35 +0700

    Learn to export and import custom components in React

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,51 +1,8 @@
-import logo from './logo.svg';
 import './App.scss';
+import Header from './Header'
+import MainContent from './MainContent';
+import Footer from './Footer';
 
-/**
-Challenge: 
-
-- Move the `footer` into its own component called "Footer" 
-  and render that component inside the Page component.
-- Move the `h1` and `ol` together into another component
-  called "MainContent" and render inside Page as well.
-*/
-
-export function Header() {
-  return (
-    <header>
-      <nav className='nav'>
-        <img className='app-logo' src={logo} alt="My App logo"></img>
-        <ul className="nav-items">
-          <li>Pricing</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
-      </nav>
-    </header>
-  )
-}
-
-export function MainContent() {
-  return (
-    <div>
-      <h1>Reasons I'm excited to learn React</h1>
-      <ol>
-        <li>It's a popular library, so I'll be
-          able to fit in with the cool kids!</li>
-        <li>I'm more likely to get a job as a developer
-          if I know React.</li>
-      </ol>
-    </div>
-  )
-}
-
-export function Footer() {
-  return (
-    <footer>
-      <p>© 2023 Luy Vannda development. All rights reserved.</p>
-    </footer>
-  )
-}
 
 export default function App() {
   return (

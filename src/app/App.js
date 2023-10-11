33c06776b8a92c52b19299fcd049115caaf8be7e
luyvannda <philips.joe329@gmail.com>
2023-10-11 10:25:10 +0700

    learn to use js inside of jsx

--- src/app/App.js
+++ src/app/App.js
@@ -2,10 +2,28 @@ import './App.scss';
 
 
 export default function App() {
-  return (
-    <div>
 
-    </div>
+  const date = new Date()
+  const hours = date.getHours()
+  let timeOfDay
+
+  if (hours < 12) {
+    timeOfDay = "morning"
+  } else if (hours >= 12 && hours < 17) {
+    timeOfDay = "afternoon"
+  } else {
+    timeOfDay = "night"
+  }
+
+  /**
+   * Challenge: fix the h1 below to use the timeOfDay
+   * string we determined in the code above
+   */
+
+  return (
+    <h1>Good {timeOfDay}!</h1>
   )
+
+
 }
 

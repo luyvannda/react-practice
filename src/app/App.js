4d34ca3c4e55302025196bbe8950d217154d6336
luyvannda <philips.joe329@gmail.com>
2023-10-11 16:02:05 +0700

    adding props to each Contact components

--- src/app/App.js
+++ src/app/App.js
@@ -1,12 +1,10 @@
 import './App.scss';
 import Card from "./components/Card.js"
 
-/* Challenge:
 
-- Create a Contact.js component in another file
-- Move one of the contact card divs below into that file
-- import and render 4 instances of that contact card
-    - Think ahead: what's the problem with doing it this way?
+/* 
+Challenge: Add all the rest of the 
+data to the Contact card instances
 */
 
 export default function App() {

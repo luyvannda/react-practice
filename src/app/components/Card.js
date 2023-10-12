76d9bd98fd8dc00b00783c347a0b1f4bb22a89cd
luyvannda <philips.joe329@gmail.com>
2023-10-12 09:09:43 +0700

    learn to use props in Contact component

--- src/app/components/Card.js
+++ src/app/components/Card.js
@@ -1,32 +1,39 @@
 import Contact from "./Contact";
-
+import whiskerson from "./images/mr-whiskerson.png"
+import fluffykins from "./images/fluffykins.png"
+import felix from "./images/felix.png"
+import pumpkin from "./images/pumpkin.png"
 
 export default function card() {
   return (
     <div className="card">
       <Contact
-        img="./images/mr-whiskerson.png"
+        img={whiskerson}
+        alt="a cat name Whiskerson"
         name="Mr. Whiskerson"
         phone="(212) 555-1234"
         email="mr.whiskaz@catnap.meow"
       />
 
       <Contact
-        img="./images/fluffykins.png"
+        img={fluffykins}
+        alt="a cat name Fluffykins"
         name="Fluffykins"
         phone="(212) 555-2345"
         email="fluff@me.com"
       />
 
       <Contact
-        img="./images/felix.png"
+        img={felix}
+        alt="a cat name Felix"
         name="Felix"
         phone="(212) 555-4567"
         email="thecat@hotmail.com"
       />
 
       <Contact
-        img="./images/pumpkin.png"
+        img={pumpkin}
+        alt="a cat name Pumpkin"
         name="Pumpkin"
         phone="(0800) CAT KING"
         email="pumpkin@scrimba.com"
--- src/app/components/Contact.js
+++ src/app/components/Contact.js
@@ -2,21 +2,21 @@ import whiskerson from "./images/mr-whiskerson.png"
 import phone from "./images/phone-icon.png"
 import email from "./images/mail-icon.png"
 
-export default function Contact() {
+export default function Contact(props) {
   return (
 
     <div className="contacts">
 
       <div className="contact-card">
-        <img src={whiskerson} alt="a cat name whiskerson" />
-        <h3>Mr. Whiskerson</h3>
+        <img src={props.img} alt={props.alt} />
+        <h3>{props.name}</h3>
         <div className="info-group">
           <img src={phone} alt="phone icon" />
-          <p>(212) 555-1234</p>
+          <p>{props.phone}</p>
         </div>
         <div className="info-group">
           <img src={email} alt="email icon" />
-          <p>mr.whiskaz@catnap.meow</p>
+          <p>{props.email}</p>
         </div>
       </div>
     </div>

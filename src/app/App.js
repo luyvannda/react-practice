d70084a90a85be88cc4602cb21b3670e081318bb
luyvannda <philips.joe329@gmail.com>
2023-10-12 11:12:53 +0700

    add props to the App components and use access it in Joke.js

--- src/app/App.js
+++ src/app/App.js
@@ -24,10 +24,25 @@ export default function App() {
 
   return (
     <div className='app'>
-      <Joke />
-      <Joke />
-      <Joke />
-      <Joke />
+      <Joke
+        setup="I got my daughter a fridge for her birthday."
+        punchline="I can't wait to see her face light up when she opens it."
+      />
+      <Joke
+        setup="How did the hacker escape the police?"
+        punchline="He just ransomware!"
+
+      />
+      <Joke
+        setup="Why don't pirates travel on mountain roads?"
+        punchline="Scurvy."
+
+      />
+      <Joke
+        setup="Why do bees stay in the hive in the winter?"
+        punchline="Swarm."
+
+      />
     </div>
   )
 }
--- src/app/components/Joke.js
+++ src/app/components/Joke.js
@@ -1,8 +1,8 @@
-export default function Joke() {
+export default function Joke(props) {
   return (
     <div className="joke">
-      <p>Setup: <span>I got my daughter a fridge for her birthday.</span></p>
-      <p>Punchline: <span>I can't wait to see her face light up when she opens it</span></p>
+      <p className="setup">Setup: <span>{props.setup}</span></p>
+      <p className="punchline">Punchline: <span>{props.punchline}</span></p>
     </div>
   )
 }

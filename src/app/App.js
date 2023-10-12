bdd5dd053bf64ea6bcb7215ee91e6a98a30744e9
luyvannda <philips.joe329@gmail.com>
2023-10-12 10:43:51 +0700

    start props practice random joke

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,33 @@
 import './App.scss';
+import Joke from './components/Joke';
+
+/* Challenge:
+- App should render 4-5 <Joke /> components 
+  (Joke component defined in its own file too)
+    - Each Joke should receive a "setup" prop and a "punchline" prop
+      and render those however you'd like
+- Use your favorite 2-part jokes (setup & punchline), or check
+  jokes.md file for some examples.
+
+EXTRA CREDIT:
+Some jokes are only a punchline with no setup:
+
+E.g.: "It’s hard to explain puns to kleptomaniacs because 
+they always take things literally."
+
+If you don't pass in a "question" prop, how might you make it only 
+show the punchline?
+*/
+
 
 export default function App() {
 
   return (
-    <div>
-
+    <div className='app'>
+      <Joke />
+      <Joke />
+      <Joke />
+      <Joke />
     </div>
   )
 }
---
+++ src/app/components/Joke.js
@@ -0,0 +1,8 @@
+export default function Joke() {
+  return (
+    <div className="joke">
+      <p>Setup: <span>I got my daughter a fridge for her birthday.</span></p>
+      <p>Punchline: <span>I can't wait to see her face light up when she opens it</span></p>
+    </div>
+  )
+}

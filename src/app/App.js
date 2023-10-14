95b50dd9aaec80ab53d7747c063fbea224dcb8d9
luyvannda <philips.joe329@gmail.com>
2023-10-14 09:29:32 +0700

    learn to use array elements in react component

--- src/app/App.js
+++ src/app/App.js
@@ -1,12 +1,22 @@
 import './App.scss';
 
+/*
+Challenge: turn the strings in the array into <h3> elements instead
+*/
 
 export default function App() {
-
+  const colors = [
+    <h3>Red</h3>,
+    <h3>Orange</h3>,
+    <h3>Yellow</h3>,
+    <h3>Green</h3>,
+    <h3>Blue</h3>,
+    <h3>Indigo</h3>,
+    <h3>Violet</h3>
+  ]
   return (
-    <div className='app'>
-
+    <div>
+      {colors}
     </div>
   )
 }
-

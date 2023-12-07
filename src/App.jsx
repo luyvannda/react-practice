2903507eba3b1c8aa60168f9e8f2247afd53619c
luyvannda <philips.joe329@gmail.com>
2023-12-07 10:48:14 +0700

    practice using Array.map and Array.push to create add new item whenever a button is clicked

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,34 @@ function App() {
   const thingsArray = ["Thing 1", "Thing 2"];
 
   /**
-   * Challenge: Map over the thingsArray to generate
-   * a <p> element for each item and render them on the page
-   * below the button
+   * Challenge: Add an event listener to the button so when
+   * it is clicked, it adds another thing to our array
+   * 
+   * Hint: use the array length + 1 to determine the number
+   * of the "Thing" being added. Also, have you event listener
+   * console.log(thingsArray) after adding the new item to the
+   * array
+   * 
+   * Spoiler: the page won't update when new things get added
+   * to the array!
    */
+
+  function mapOver() {
+    return thingsArray.map((thing, index) => (
+      <p key={index}> {thing}</p>
+    ));
+  }
+
+  function addThing() {
+    const newThing = `Thing ${thingsArray.length + 1}`;
+    thingsArray.push(newThing);
+    console.log(thingsArray);
+  }
 
   return (
     <div>
-      <button>Add Item</button>
-      {/* Insert the things here*/}
+      <button onClick={addThing}>Add Item</button>
+      {mapOver()}
     </div>
   )
 }

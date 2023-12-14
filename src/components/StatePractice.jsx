f82b05a9211d6736292eb7341656d1436496baf0
luyvannda <philips.joe329@gmail.com>
2023-12-14 15:32:16 +0700

    use spread operator to update state array

--- src/components/StatePractice.jsx
+++ src/components/StatePractice.jsx
@@ -1,28 +1,16 @@
 import { useState } from "react"
 
-
 export default function StatePractice() {
-  const [isGoingOut, setIsGoingOut] = useState(true)
 
-  /**
-     * Challenge: Convert the code below to use an array
-     * held in state instead of a local variable. Initialize 
-     * the state array with the same 2 items below
-     * 
-     * Don't worry about fixing `addItem` quite yet.
-     */
-  const thingsArray = ["Thing 1", "Thing 2"]
+  const [thingsArray, setThingsArray] = useState(["Thing 1", "Thing 2"])
 
   function addItem() {
-    // We'll work on this next
-    // const newThingText = `Thing ${thingsArray.length + 1}`
-    // thingsArray.push(newThingText)
-    // document.getElementById()
-    // console.log(thingsArray)
+    const newThingsArray = `Thing ${thingsArray.length + 1}`;
+
+    setThingsArray((prevThingsArray) => [...prevThingsArray, newThingsArray])
   }
 
   const thingsElements = thingsArray.map(thing => <p key={thing}>{thing}</p>)
-
 
   return (
     <>

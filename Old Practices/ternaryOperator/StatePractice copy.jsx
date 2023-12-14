fe314afab626f02bf4cf97738f6f98e03cb15909
luyvannda <philips.joe329@gmail.com>
2023-12-14 14:58:56 +0700

    practice complex react state with arrays

---
+++ Old Practices/ternaryOperator/StatePractice copy.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react"
+
+/**
+   * Challenge: 
+   * - Initialize state for `isGoingOut` as a boolean
+   * - Make it so clicking the div.state--value flips that
+   *   boolean value (true -> false, false -> true)
+   * - Display "Yes" if `isGoingOut` is `true`, "No" otherwise
+   */
+
+export default function StatePractice() {
+  const [isGoingOut, setIsGoingOut] = useState(true)
+
+  function handleClick() {
+    setIsGoingOut((going) => !going)
+  }
+
+  return (
+    <>
+
+      <div className="state">
+        <h1 className="state--title">Do I feel like going out tonight?</h1>
+        <button onClick={handleClick} className="state--value">
+          {isGoingOut ? "Yes" : "No"}
+        </button>
+      </div>
+
+    </>
+  )
+}
--- src/components/StatePractice.jsx
+++ src/components/StatePractice.jsx
@@ -1,30 +1,34 @@
 import { useState } from "react"
 
 
-/**
-   * Challenge: 
-   * - Initialize state for `isGoingOut` as a boolean
-   * - Make it so clicking the div.state--value flips that
-   *   boolean value (true -> false, false -> true)
-   * - Display "Yes" if `isGoingOut` is `true`, "No" otherwise
-   */
-
-
 export default function StatePractice() {
   const [isGoingOut, setIsGoingOut] = useState(true)
 
-  function handleClick() {
-    setIsGoingOut((going) => !going)
+  /**
+     * Challenge: Convert the code below to use an array
+     * held in state instead of a local variable. Initialize 
+     * the state array with the same 2 items below
+     * 
+     * Don't worry about fixing `addItem` quite yet.
+     */
+  const thingsArray = ["Thing 1", "Thing 2"]
+
+  function addItem() {
+    // We'll work on this next
+    // const newThingText = `Thing ${thingsArray.length + 1}`
+    // thingsArray.push(newThingText)
+    // document.getElementById()
+    // console.log(thingsArray)
   }
+
+  const thingsElements = thingsArray.map(thing => <p key={thing}>{thing}</p>)
+
 
   return (
     <>
-
-      <div className="state">
-        <h1 className="state--title">Do I feel like going out tonight?</h1>
-        <button onClick={handleClick} className="state--value">
-          {isGoingOut ? "Yes" : "No"}
-        </button>
+      <div>
+        <button onClick={addItem}>Add Item</button>
+        {thingsElements}
       </div>
 
     </>

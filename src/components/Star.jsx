280c07118bfeaff64fd87760def59daa962d15db
luyvannda <philips.joe329@gmail.com>
2023-12-18 10:25:06 +0700

    address issue with accessibiilty in Star component

--- src/components/Star.jsx
+++ src/components/Star.jsx
@@ -1,17 +1,29 @@
 import { PropTypes } from "prop-types"
 
+/**
+  * Challenge:
+  * If the star is filled, add an aria-label of "Unmark as favorite".
+  * Otherwise, use the aria-label of "Mark as favorite".
+  */
+
 export default function Star(props) {
   let starIcon = props.isFilled ? "star-filled.png" : "star-empty.png"
+  let buttonLabel = props.isFilled ? "Unmark as favorite" : "Mark as favorite"
 
   return (
     <>
-
-      <img
-        src={starIcon}
-        className="card--favorite"
+      <button
         onClick={props.handleClick}
-      />
-
+        aria-label={buttonLabel}
+        aria-pressed={props.isFilled}
+        className="card--favorite-button"
+      >
+        <img
+          src={starIcon}
+          alt="Star Icon."
+          className="card--favorite"
+        />
+      </button>
     </>
   )
 }

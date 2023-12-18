54c4f096ac3823a2fccab04a459ef2667118ec30
luyvannda <philips.joe329@gmail.com>
2023-12-18 15:30:44 +0700

    learn to set inline style with ternary ooperator and props

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,38 @@
 import './App.scss'
 import { useState } from "react"
 import Boxes from './components/Boxes'
+import { PropTypes } from "prop-types"
 
 
-function App() {
-
-  /**
-       * Challenge part 1:
-       * 1. Initialize state with the default value of the
-       *    array pulled in from boxes.js
-       * 2. Map over that state array and display each one
-       *    as an empty square (black border, transparent bg color)
-       *    (Don't worry about using the "on" property yet)
-       */
+function App(props) {
 
   const [squares, setSquares] = useState(Boxes)
 
+  // Challenge: use a ternary to determine the backgroundColor.
+  // If darkMode is true, set it to "#222222"
+  // If darkMode is false, set it to "#cccccc"
+
+  const styles = {
+    backgroundColor: props.darkMode ? "#222222" : "#cccccc"
+  }
+
+
   const squareElements = squares.map(squares => (
-    <div className='box' key={squares.id}>
+    <div style={styles} className='box' key={squares.id}>
     </div>
   ))
-
-  function handleClick() {
-    console.log(squares);
-  }
 
   return (
 
     <main>
-
       {squareElements}
-
     </main>
 
   )
+}
+
+App.propTypes = {
+  darkMode: PropTypes.bool.isRequired
 }
 
 export default App

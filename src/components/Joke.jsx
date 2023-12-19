ca0e75ca908f3c3126fd4bc875052fe160cbc68c
luyvannda <philips.joe329@gmail.com>
2023-12-19 15:39:02 +0700

    complete another challenge to use && for conditional rendering

--- src/components/Joke.jsx
+++ src/components/Joke.jsx
@@ -1,17 +1,25 @@
 import { PropTypes } from "prop-types"
-
+import { useState } from "react"
 
 export default function Joke(props) {
   /**
-   * Challenge:
-   * - Create state `isShown` (boolean, default to `false`)
-   * - Add a button that toggles the value back and forth
-   */
+    * Challenge:
+    * - Only display the punchline paragraph if `isShown` is true
+    */
+
+  const [isShown, setIsShown] = useState(false);
+
+  function toggle() {
+
+    setIsShown(prevShown => !prevShown)
+  }
+
   return (
     <div>
       {props.setup && <h3>{props.setup}</h3>}
-      <p>{props.punchline}</p>
+      {isShown && <p>{props.punchline}</p>}
 
+      <button onClick={toggle}>Show punchline</button>
       <hr />
     </div>
   )

635bf4dc41fbf26c43186eba95dfb7d43885aa2b
luyvannda <philips.joe329@gmail.com>
2023-12-19 10:06:17 +0700

    learn to use derived state control

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,8 @@ import Box from './components/Box'
 
 function App() {
 
+  // eslint-disable-next-line no-unused-vars
   const [squares, setSquares] = useState(Boxes)
-
-  /**
-     * Challenge part 2:
-     * 1. Create a separate component called "Box" and
-     *    replace the `div` above with our <Box /> components
-     * 2. Pass the Box component a prop called `on` with the
-     *    value of the same name from the `boxes` objects
-     * 3. In the Box component, apply dynamic styles to determine
-     *    the backgroundColor of the box. If it's `on`, set the
-     *    backgroundColor to "#222222". If off, set it to "none"
-     */
 
   const squareElements = squares.map(squares => (
     <Box key={squares.id} on={squares.on} />
--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,15 +1,35 @@
 import { PropTypes } from "prop-types"
+import { useState } from "react"
+
+/**
+    * Challenge: Create state controlling whether
+    * this box is "on" or "off". Use the incoming
+    * `props.on` to determine the initial state.
+    * 
+    * Create an event listener so when the box is clicked,
+    * it toggles from "on" to "off".
+    * 
+    * Goal: clicking each box should toggle it on and off.
+    */
+
 
 export default function Box(props) {
 
+  const [control, setControl] = useState(props.on)
+
+  function handleClick() {
+
+    setControl(prevControl => (prevControl ? false : true))
+  }
+
   const styles = {
-    backgroundColor: props.on ? "#222222" : "none"
+    backgroundColor: control ? "#222222" : "transparent"
   }
 
   return (
     <>
-      <div style={styles} className='box' >
-      </div>
+      <button onClick={handleClick} style={styles} className='box' >
+      </button>
     </>
   )
 }

4b977a8e7ce7f853e7662e91112f6ac85f5eb9c5
luyvannda <philips.joe329@gmail.com>
2023-12-25 10:49:49 +0700

    start learning about useEffect hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
 import './App.scss'
 // import Form from './components/Form'
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 export default function App() {
   const [starWarsData, setStarWarsData] = useState({})
+  const [count, setCount] = useState(0);
 
-  fetch("https://swapi.dev/api/people/1")
-    .then(res => res.json())
-    .then(data => setStarWarsData(data))
+  console.log("Component Rendered")
+
+  useEffect(function () {
+
+    console.log("Run Effect")
+    // fetch("https://swapi.dev/api/people/1")
+    //   .then(res => res.json())
+    //   .then(data => setStarWarsData(data))
+
+  }, [count])
+
 
   return (
     <div>
       <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
+      <h2>The count is {count}</h2>
+      <button className='btn btn-primary' onClick={() => setCount(prevCount => prevCount + 1)}>Add</button>
     </div>
   )
 }

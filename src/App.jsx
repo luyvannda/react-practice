1216f6521d0f0d86fd94ad8d9f17280d7406399c
luyvannda <philips.joe329@gmail.com>
2023-12-25 16:02:22 +0700

    complete the lesson to learn to fetch data from api using useEffect to prevent side effect from re-rendering infinitely

--- src/App.jsx
+++ src/App.jsx
@@ -4,25 +4,25 @@ import { useState, useEffect } from "react"
 
 export default function App() {
   const [starWarsData, setStarWarsData] = useState({})
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
-  console.log("Component Rendered")
 
   useEffect(() => {
 
     console.log("Run Effect")
-    // fetch("https://swapi.dev/api/people/1")
-    //   .then(res => res.json())
-    //   .then(data => setStarWarsData(data))
 
-  }, [count])
+    fetch("https://swapi.dev/api/people/1")
+      .then(res => res.json())
+      .then(data => setStarWarsData(data))
+
+  }, [])
 
 
   return (
     <div>
       <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
-      <h2>The count is {count}</h2>
-      <button className='btn btn-primary' onClick={() => setCount(prevCount => prevCount + 1)}>Add</button>
+      {/* <h2>The count is {count}</h2>
+      <button className='btn btn-primary' onClick={() => setCount(prevCount => prevCount + 1)}>Add</button> */}
     </div>
   )
 }

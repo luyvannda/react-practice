227f03cdd7478c2472b32f80bb7cc6986b34487a
luyvannda <philips.joe329@gmail.com>
2023-12-26 15:21:16 +0700

    make so that when window width change it will update the state and display it in real time

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,12 @@ import { useState } from "react"
 
 
 export default function App() {
-  /**
-   * Challenge:
-   * 1. Create state called `show`, default to `true`
-   * 2. When the button is clicked, toggle `show`
-   * 3. Only display `<WindowTracker>` if `show` is `true`
-   */
 
   const [show, setShow] = useState(true);
 
   function toggle() {
     setShow(prevShow => !prevShow)
   }
-  console.log(show)
 
   return (
     <div className="container">
--- src/components/WindowTracker.jsx
+++ src/components/WindowTracker.jsx
@@ -1,9 +1,28 @@
-
+import { useState, useEffect } from "react"
 
 export default function WindowTracker() {
 
+  /**
+       * Challenge:
+       * 1. Create state called `windowWidth`, default to 
+       *    `window.innerWidth`
+       * 2. When the window width changes, update the state
+       * 3. Display the window width in the h1 so it updates
+       *    every time it changes
+       */
+
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth)
+
+  useEffect(() => {
+    window.addEventListener("resize", function () {
+
+      setWindowWidth(window.innerWidth)
+
+    })
+  }, [])
+
   return (
-    <h1>Window width: {window.innerWidth}</h1>
+    <h1>Window width: {windowWidth}</h1>
   )
 }
 

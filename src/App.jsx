57f09cf97cae6746299952431356a0a0a7099483
luyvannda <philips.joe329@gmail.com>
2023-12-26 10:57:34 +0700

    initialize new challenge on state and effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,22 @@
 import './App.scss'
-// import Form from './components/Form'
-import { useState, useEffect } from "react"
-
-/**
-     * Challenge: Combine `count` with the request URL
-     * so pressing the "Get Next Character" button will
-     * get a new character from the Star Wars API.
-     * Remember: don't forget to consider the dependencies
-     * array!
-     */
+import WindowTracker from './components/WindowTracker'
+// import { useState, useEffect } from "react"
 
 
 export default function App() {
-  const [starWarsData, setStarWarsData] = useState({})
-  const [count, setCount] = useState(1);
-
-  useEffect(() => {
-
-    console.log("Run Effect")
-
-    fetch(`https://swapi.dev/api/people/${count}`)
-      .then(res => res.json())
-      .then(data => setStarWarsData(data))
-
-  }, [count])
-
+  /**
+   * Challenge:
+   * 1. Create state called `show`, default to `true`
+   * 2. When the button is clicked, toggle `show`
+   * 3. Only display `<WindowTracker>` if `show` is `true`
+   */
 
   return (
-    <div>
-      <h2>The count is {count}</h2>
-      <button className='btn btn-primary' onClick={() => setCount(prevCount => prevCount + 1)}>Get next character!</button>
-
-      <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
-
+    <div className="container">
+      <button>
+        Toggle WindowTracker
+      </button>
+      <WindowTracker />
     </div>
   )
 }
---
+++ src/components/WindowTracker.jsx
@@ -0,0 +1,14 @@
+
+
+export default function WindowTracker() {
+
+  return (
+    <h1>Window width: {window.innerWidth}</h1>
+  )
+}
+
+
+// Form.propTypes = {
+//   setup: PropTypes.string.isRequired,
+//   punchline: PropTypes.string.isRequired
+// }

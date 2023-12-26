4ee24316ccde479a6ec0fd49ead7ca7d8a488f47
luyvannda <philips.joe329@gmail.com>
2023-12-26 15:26:23 +0700

    add cleanning up code for useEffect function in case the component did unmount

--- src/components/WindowTracker.jsx
+++ src/components/WindowTracker.jsx
@@ -2,23 +2,22 @@ import { useState, useEffect } from "react"
 
 export default function WindowTracker() {
 
-  /**
-       * Challenge:
-       * 1. Create state called `windowWidth`, default to 
-       *    `window.innerWidth`
-       * 2. When the window width changes, update the state
-       * 3. Display the window width in the h1 so it updates
-       *    every time it changes
-       */
 
   const [windowWidth, setWindowWidth] = useState(window.innerWidth)
 
   useEffect(() => {
-    window.addEventListener("resize", function () {
 
+    function watchWidth() {
+      console.log("Setting")
       setWindowWidth(window.innerWidth)
+    }
+    window.addEventListener("resize", watchWidth)
 
-    })
+    return function () {
+      console.log("Cleaning up")
+      window.removeEventListener("resize", watchWidth)
+    }
+
   }, [])
 
   return (

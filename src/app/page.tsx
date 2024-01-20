bccd5ea3aef579b5426ae1c7248f591e9ef5e9ad
luyvannda <philips.joe329@gmail.com>
2024-01-20 11:09:46 +0700

    add new react app on nextjs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
 export default function Home() {
-  return <main></main>;
+  async function getAdvice() {}
+
+  return (
+    <main>
+      <h1 className="text-3xl font-semibold">Hello World</h1>
+      <button className="bg-sky-400 rounded py-1 px-2 hover:bg-sky-600 hover:text-white">
+        Get advice
+      </button>
+    </main>
+  );
 }

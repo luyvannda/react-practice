62a91d22829e439b2daa3a06b4d966c91536a4b2
luyvannda <philips.joe329@gmail.com>
2024-01-22 13:09:02 +0700

    initialize a new vite tailwind work flow

--- ReactJS/src/components/Main.jsx
+++
@@ -1,9 +0,0 @@
-// import { PropTypes } from "prop-types"
-
-export default function Main() {
-  return <div></div>;
-}
-
-// Main.propTypes = {
-//   darkMode: PropTypes.bool.isRequired
-// }
---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+// import { useState } from "react";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="">
+      <h1 className="text-4xl font-semibold underline">Hello World</h1>
+    </div>
+  );
+}

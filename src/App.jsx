aa97088aa4a802d9dda0df15bdb43263eaa96c9c
Luy Vannda <philips.joe329@gmail.com>
2024-01-28 12:26:45 +0700

    remove bootstrap from old practice

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,6 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 import "./App.css";
-import Message from "./components/Message";
 
 export default function App() {
-  const [advice, setAdvice] = useState("");
-  const [count, setCount] = useState(0);
-
-  async function getAdvice() {
-    const res = await fetch("https://api.adviceslip.com/advice");
-    const data = await res.json();
-    setAdvice(data.slip.advice);
-    setCount((count) => count + 1);
-  }
-
-  useEffect(function () {
-    getAdvice();
-  }, []);
-
-  return (
-    <div className="pt-6 pl-4 text-2xl text-center space-y-5">
-      <h1 className="font-semibold">{advice}</h1>
-      <button
-        onClick={getAdvice}
-        className="px-3 bg-blue-400 text-white rounded-lg hover:bg-blue-500 focus:outline-slate-500 active:bg-blue-600 shadow-md"
-      >
-        Get Advice
-      </button>
-
-      <Message count={count} />
-    </div>
-  );
+  return <div></div>;
 }

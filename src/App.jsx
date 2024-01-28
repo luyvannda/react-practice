54c56d938d107fcfd2b08159e1c87bc2e1056fff
Luy Vannda <philips.joe329@gmail.com>
2024-01-28 15:20:56 +0700

    complete our ToDos component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 // import { useEffect, useState } from "react";
 import "./App.css";
 import ToDos from "./components/ToDos";
+import { RecoilRoot } from "recoil";
 
 export default function App() {
   return (
-    <div className="w-screen h-screen">
-      <div className="container mx-4 flex flex-col justify-center items-center">
-        <h1>Hello</h1>
-        <ToDos />
+    <RecoilRoot>
+      <div className="w-screen h-screen">
+        <div className="container mx-4 flex flex-col justify-center items-center">
+          <h1>Hello</h1>
+          <ToDos />
+        </div>
       </div>
-    </div>
+    </RecoilRoot>
   );
 }
---
+++ src/components/ToDos.jsx
@@ -0,0 +1,48 @@
+import { useRecoilState } from "recoil";
+import { todoState } from "../state/atoms/TodoState";
+import { useState } from "react";
+
+export default function ToDos() {
+  const [todos, setToDos] = useRecoilState(todoState);
+  const [inputText, setInputText] = useState("");
+
+  function addToDo() {
+    setToDos((prevToDos) => [
+      ...prevToDos,
+      {
+        id: prevToDos.length + 1,
+        text: inputText
+      }
+    ]);
+
+    setInputText("");
+  }
+
+  console.log({ todos });
+
+  function handleInputChange(e) {
+    setInputText(e.target.value);
+  }
+
+  function clearToDo() {
+    setToDos([]);
+  }
+
+  return (
+    <main>
+      <input
+        value={inputText}
+        onChange={handleInputChange}
+        className="outline outline-cyan-900"
+        type="text"
+        id="todoInput"
+      />
+      <button onClick={addToDo} className="w-28 bg-blue-400">
+        Add Todo
+      </button>
+      <button onClick={clearToDo} className="w-28 bg-slate-300">
+        Clear Todos
+      </button>
+    </main>
+  );
+}

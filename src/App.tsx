82f0056a1266f5044860d90a6a30682dcf80ffb8
luyvannda <luyvannda@gmail.com>
2024-03-07 10:01:48 +0700

    create a static steps component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,32 @@
 import "./App.css";
 
+const messages: string[] = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
 function App() {
-  return <div>Hello World</div>;
+  const step = 1;
+
+  return (
+    <div className="steps">
+      <div className="numbers">
+        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+      </div>
+
+      <p className="message">
+        Step {step}: {messages[step - 1]}
+      </p>
+
+      <div className="buttons">
+        <button className="bg-[#7950f2] text-white">Previous</button>
+        <button className="bg-[#7950f2] text-white">Next</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;

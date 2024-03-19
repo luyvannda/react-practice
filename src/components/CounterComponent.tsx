73e9838a8130a6ed08faf03ab0587c22ba9ca41a
luyvannda <luyvannda@gmail.com>
2024-03-19 08:56:58 +0700

    add animation and state to CountComponent

--- src/components/CounterComponent.tsx
+++ src/components/CounterComponent.tsx
@@ -1,4 +1,31 @@
-const CounterComponent = () => {
+import { FC, useState } from "react";
+
+interface CounterComponentProps {
+  count: number;
+  onCountChange: (newCount: number) => void;
+}
+
+const CounterComponent: FC<CounterComponentProps> = ({
+  count,
+  onCountChange,
+}) => {
+  const [minusWiggling, setMinusWiggling] = useState(false);
+  const [plusWiggling, setPlusWiggling] = useState(false);
+
+  function handleMinus() {
+    setMinusWiggling(true);
+    if (count > -30 && count <= 30) {
+      onCountChange(count - 1);
+    }
+  }
+
+  function handlePlus() {
+    setPlusWiggling(true);
+    if (count >= -30 && count < 30) {
+      onCountChange(count + 1);
+    }
+  }
+
   return (
     <>
       <div className="flex gap-2">
@@ -9,7 +36,7 @@ const CounterComponent = () => {
         >
           -
         </button>
-        <p>Counter: {}</p>
+        <p>Count: {count}</p>
         <button
           onClick={handlePlus}
           className={`bg-gray-300 px-2 ${plusWiggling ? "animate-wiggle" : ""} `}
--- src/components/DateCounter.tsx
+++ src/components/DateCounter.tsx
@@ -1,16 +1,26 @@
 import { useState } from "react";
 import StepComponent from "./StepComponent";
+import CounterComponent from "./CounterComponent";
 
 export default function DateCounter() {
   const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
 
   const handleStepChange = (newStep: number) => {
     setStep(newStep);
   };
 
+  const handleCountChange = (newCount: number) => {
+    setCount(newCount);
+  };
+
+  const today = new Date();
+
   return (
     <div className="flex flex-col gap-4">
       <StepComponent step={step} onStepChange={handleStepChange} />
+      <CounterComponent count={count} onCountChange={handleCountChange} />
+      <p>Today is {`${today}`}</p>
     </div>
   );
 }

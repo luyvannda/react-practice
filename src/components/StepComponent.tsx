2ae88117e18349783674ae448ee74996971398cf
luyvannda <luyvannda@gmail.com>
2024-03-28 16:41:33 +0700

    add slider input to steps component

--- src/components/StepComponent.tsx
+++ src/components/StepComponent.tsx
@@ -1,4 +1,4 @@
-import { FC, useState } from "react";
+import { FC } from "react";
 
 interface StepComponentProps {
   step: number;
@@ -6,42 +6,61 @@ interface StepComponentProps {
 }
 
 const StepComponent: FC<StepComponentProps> = ({ step, onStepChange }) => {
-  const [minusWiggling, setMinusWiggling] = useState(false);
-  const [plusWiggling, setPlusWiggling] = useState(false);
+  // const [minusWiggling, setMinusWiggling] = useState(false);
+  // const [plusWiggling, setPlusWiggling] = useState(false);
 
-  function handleMinus() {
-    setMinusWiggling(true);
-    if (step > 1) {
-      onStepChange(step - 1);
-    }
-  }
+  // function handleMinus() {
+  //   setMinusWiggling(true);
+  //   if (step > 1) {
+  //     onStepChange(step - 1);
+  //   }
+  // }
 
-  function handlePlus() {
-    setPlusWiggling(true);
-    if (step < 100) {
-      onStepChange(step + 1);
-    }
-  }
+  // function handlePlus() {
+  //   setPlusWiggling(true);
+  //   if (step < 100) {
+  //     onStepChange(step + 1);
+  //   }
+  // }
+
+  type eType = {
+    target: {
+      value: string;
+    };
+  };
+
+  const handleChange = (e: eType): void => {
+    onStepChange(parseInt(e.target.value));
+  };
 
   return (
     <>
       <div className="flex gap-2">
-        <button
+        <input
+          onChange={handleChange}
+          type="range"
+          min={0}
+          max={10}
+          defaultValue={step}
+        />
+
+        {/* <button
           onClick={handleMinus}
           className={`bg-gray-300 px-2 hover:bg-gray-400 ${minusWiggling ? "animate-wiggle" : ""} `}
           onAnimationEnd={() => setMinusWiggling(false)}
         >
           -
-        </button>
-        <p>Step: {step}</p>
+        </button> */}
 
-        <button
+        <p>{step}</p>
+
+        {/* <button
           onClick={handlePlus}
           className={`bg-gray-300 px-2 hover:bg-gray-400 ${plusWiggling ? "animate-wiggle" : ""} `}
           onAnimationEnd={() => setPlusWiggling(false)}
         >
           +
-        </button>
+        </button> */}
       </div>
     </>
   );

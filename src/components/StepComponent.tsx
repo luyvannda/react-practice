a0bfdf1f6d91263d35c6f5decb404e9d3735518a
luyvannda <luyvannda@gmail.com>
2024-03-28 16:45:20 +0700

    clean up the old cold from date counter v1

--- src/components/StepComponent.tsx
+++ src/components/StepComponent.tsx
@@ -6,23 +6,6 @@ interface StepComponentProps {
 }
 
 const StepComponent: FC<StepComponentProps> = ({ step, onStepChange }) => {
-  // const [minusWiggling, setMinusWiggling] = useState(false);
-  // const [plusWiggling, setPlusWiggling] = useState(false);
-
-  // function handleMinus() {
-  //   setMinusWiggling(true);
-  //   if (step > 1) {
-  //     onStepChange(step - 1);
-  //   }
-  // }
-
-  // function handlePlus() {
-  //   setPlusWiggling(true);
-  //   if (step < 100) {
-  //     onStepChange(step + 1);
-  //   }
-  // }
-
   type eType = {
     target: {
       value: string;
@@ -44,23 +27,7 @@ const StepComponent: FC<StepComponentProps> = ({ step, onStepChange }) => {
           defaultValue={step}
         />
 
-        {/* <button
-          onClick={handleMinus}
-          className={`bg-gray-300 px-2 hover:bg-gray-400 ${minusWiggling ? "animate-wiggle" : ""} `}
-          onAnimationEnd={() => setMinusWiggling(false)}
-        >
-          -
-        </button> */}
-
         <p>{step}</p>
-
-        {/* <button
-          onClick={handlePlus}
-          className={`bg-gray-300 px-2 hover:bg-gray-400 ${plusWiggling ? "animate-wiggle" : ""} `}
-          onAnimationEnd={() => setPlusWiggling(false)}
-        >
-          +
-        </button> */}
       </div>
     </>
   );

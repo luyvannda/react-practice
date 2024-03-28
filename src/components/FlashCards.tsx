25eac91c44b6f4bde176de1d39c183f70ef522b6
luyvannda <luyvannda@gmail.com>
2024-03-28 10:30:14 +0700

    use union type to style flash card component based on the selectedId with type number or null

--- src/components/FlashCards.tsx
+++ src/components/FlashCards.tsx
@@ -1,9 +1,20 @@
+import { useState } from "react";
 import questions from "./questions";
 
 export default function FlashCards() {
+  const [selectedId, setSelectedId] = useState<number | null>(7336);
+
+  const handleClick = (id: number | null) => {
+    setSelectedId(id !== selectedId ? id : null);
+  };
+
   const questionElements = questions.map((question) => (
-    <div key={question.id}>
-      <p>{question.question}</p>
+    <div
+      key={question.id}
+      onClick={() => handleClick(question.id)}
+      className={question.id === selectedId ? "selected" : ""}
+    >
+      <p>{question.id === selectedId ? question.answer : question.question}</p>
     </div>
   ));
   return <div className="flashcards">{questionElements}</div>;
